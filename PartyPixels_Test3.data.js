fbe25f2d3910f4d853add7194c1f72d9422f83ed
dante6686-pp <dante6686@gmail.com>
2025-12-19 12:11:48 +0100

    added loading screen and hid buttons

--- PartyPixels_Test3.data.js
+++ PartyPixels_Test3.data.js
@@ -173,6 +173,6 @@ Module['FS_createPath']('/PartyPixels_Test3/Content', 'Paks', true, true);
   }
 
  }
- loadPackage({"files": [{"start": 0, "audio": 0, "end": 53, "filename": "/Manifest_NonUFSFiles_HTML5.txt"}, {"start": 53, "audio": 0, "end": 107, "filename": "/UE4CommandLine.txt"}, {"start": 107, "audio": 0, "end": 92263978, "filename": "/PartyPixels_Test3/Content/Paks/PartyPixels_Test3-HTML5.pak"}], "remote_package_size": 92263978, "package_uuid": "32d339cf-8b2c-478c-b1dc-f2186fd670e5"});
+ loadPackage({"files": [{"start": 0, "audio": 0, "end": 53, "filename": "/Manifest_NonUFSFiles_HTML5.txt"}, {"start": 53, "audio": 0, "end": 107, "filename": "/UE4CommandLine.txt"}, {"start": 107, "audio": 0, "end": 92263990, "filename": "/PartyPixels_Test3/Content/Paks/PartyPixels_Test3-HTML5.pak"}], "remote_package_size": 92263990, "package_uuid": "c879f390-9cf3-424d-aa72-1ee20c0e5273"});
 
 })();
